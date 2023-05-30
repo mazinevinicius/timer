54b69ea8ee92951085c9b7886ada350a8e8f93cf
Vinicius Mazine <vinicius.marques@gazin.com.br>
2023-05-30 16:43:34 -0300

    change configs

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'api/_build', // original value 'build'
+  devServerPort: 8002,
   ignoredRouteFiles: ["**/.*"],
   // When running locally in development mode, we use the built-in remix
   // server. This does not understand the vercel lambda module format,
